0306e6f41d7ac85d176ddcc712a82c81624d1f7a
whoismissing <whereismissing@gmail.com>
2021-07-14 20:10:39 -0400

    fixed draw display, achieved bare minimum IBM logo

--- chip8.c
+++ chip8.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
 #define SUCCESS 0
 #define FAILURE 1
 #define MAX_RAM 4096
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
+#define MAX_REG 15
 
 typedef struct Chip8 {
     int16_t PC;
@@ -21,10 +21,10 @@ typedef struct Chip8 {
 
 Chip8 CHIP8 = { 0x200, 0, { '\0' }, 0, 0 };
 
-uint8_t RAM[MAX_RAM] = { '\0' };
-int8_t DISPLAY[64][32] = { '\0' };
+u_int8_t RAM[MAX_RAM] = { '\0' };
+int8_t DISPLAY[DISPLAY_WIDTH * DISPLAY_HEIGHT] = { '\0' }; // 64 pixels wide 32 pixels tall
 
-uint8_t FONT[] = {
+u_int8_t FONT[] = {
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
     0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -43,44 +43,52 @@ uint8_t FONT[] = {
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-void DumpHex(const void* data, size_t size) {
-    char ascii[17];
-    size_t i, j;
-    ascii[16] = '\0';
-    for (i = 0; i < size; ++i) {
-        printf("%02X ", ((unsigned char*)data)[i]);
-        if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
-            ascii[i % 16] = ((unsigned char*)data)[i];
-        } else {
-            ascii[i % 16] = '.';
-        }
-        if ((i+1) % 8 == 0 || i+1 == size) {
-            printf(" ");
-            if ((i+1) % 16 == 0) {
-                printf("|  %s \n", ascii);
-            } else if (i+1 == size) {
-                ascii[(i+1) % 16] = '\0';
-                if ((i+1) % 16 <= 8) {
-                    printf(" ");
-                }
-                for (j = (i+1) % 16; j < 16; ++j) {
-                    printf("   ");
-                }
-                printf("|  %s \n", ascii);
-            }
-        }
-    }
+void draw() {
+	for (u_int8_t y = 0; y < DISPLAY_HEIGHT; y++) {
+		for (u_int8_t x = 0; x < DISPLAY_WIDTH; x++) {
+			if (DISPLAY[x + y * DISPLAY_WIDTH]) {
+				printf("%d", DISPLAY[x + y * DISPLAY_WIDTH]);
+			} else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
 }
 
-void draw_display() {
+void draw_display(u_int16_t X, u_int16_t Y, u_int16_t N) {
+    u_int16_t VX = CHIP8.VR[X];
+    u_int16_t VY = CHIP8.VR[Y];
+
+	u_int32_t offset;
+    u_int8_t sprite = 0;
+
+    CHIP8.VR[0xF] &= 0; // set VF to 0
+
+    for (u_int16_t y = 0; y < N; y++) {
+
+        sprite = RAM[CHIP8.I + y];
+
+		for (u_int8_t x = 0; x < 8; x++) {
+
+			offset = x + VX + ((y + VY) * DISPLAY_WIDTH);
+
+			if (sprite & (0x80 >> x)) {
+				if (DISPLAY[offset]) {
+					DISPLAY[offset] = 0;
+					CHIP8.VR[0xF] = 1;
+				} else {
+					DISPLAY[offset] ^= 1;
+				}
+			}
+		}
+	} // end for
+}
+
+void clear_display() {
     int x;
     int y;
-    for (x = 0; x < DISPLAY_WIDTH; x++) {
-        for (y = 0; y < DISPLAY_HEIGHT; y++) {
-            printf("%d", DISPLAY[x][y]);
-        }
-        printf("\n");
-    }
+	memset(&DISPLAY, '\0', sizeof(DISPLAY));
 }
 
 int load_rom(char * filename) {
@@ -98,6 +106,10 @@ int load_rom(char * filename) {
     unsigned int sz = ftell(fp);
     rewind(fp);
 
+	if (sz < 0 || sz >= MAX_RAM - 0x200) {
+		fprintf(stderr, "bad rom size %d\n", sz);
+		exit(FAILURE);
+	}
     // copy byte-code into RAM starting at 0x200
     fread(&RAM[0x200], sz, 1, fp);
 
@@ -108,10 +120,10 @@ Exit:
     return ret_val; 
 }
 
-uint16_t fetch() {
-    uint16_t op_hi = RAM[CHIP8.PC] << 8;
-    uint16_t op_lo = RAM[CHIP8.PC + 1];
-    uint16_t opcode = op_hi | op_lo;
+u_int16_t fetch() {
+    u_int16_t op_hi = RAM[CHIP8.PC] << 8;
+    u_int16_t op_lo = RAM[CHIP8.PC + 1];
+    u_int16_t opcode = op_hi | op_lo;
 
     fprintf(stderr, "pc = 0x%x opcode = %04X\n", CHIP8.PC, opcode);
     CHIP8.PC += 2;
@@ -123,20 +135,29 @@ uint16_t fetch() {
     return opcode;
 }
 
-uint16_t decode(uint16_t opcode, const char * value) {
-    uint16_t decoded = 0xFFFF;
+void verify_register_index(u_int16_t index) {
+	if (index < 0 || index > MAX_REG) {
+		fprintf(stderr, "register index %d is out of bounds!\n", index);
+		exit(FAILURE);
+	}
+}
+
+u_int16_t decode(u_int16_t opcode, const char * value) {
+    u_int16_t decoded = 0xFFFF;
     if (strcmp(value, "op") == 0) {
         decoded = (opcode & 0xF000) >> 12;
     } else if (strcmp(value, "X") == 0) {
         decoded = (opcode & 0x0F00) >> 8; // index to lookup register V0-VF
+		verify_register_index(decoded);
     } else if (strcmp(value, "Y") == 0) {
         decoded = (opcode & 0x00F0) >> 4; // index to lookup register V0-VF
+		verify_register_index(decoded);
     } else if (strcmp(value, "N") == 0) {
-        decoded = (opcode & 0x000F); // 4-bit number
+        decoded = (opcode & 0x000F);      // 4-bit number
     } else if (strcmp(value, "NN") == 0) {
-        decoded = (opcode & 0x00FF); // 8-bit immediate number
+        decoded = (opcode & 0x00FF);      // 8-bit immediate number
     } else if (strcmp(value, "NNN") == 0) {
-        decoded = (opcode & 0x0FFF); // 12-bit immediate memory address
+        decoded = (opcode & 0x0FFF);      // 12-bit immediate memory address
     } else {
         fprintf(stderr, "Unknown value %s for decode option\n", value);
         exit(FAILURE);
@@ -146,21 +167,19 @@ uint16_t decode(uint16_t opcode, const char * value) {
 }
 
 void emulate() {
-    int i = 0;
     while (1) {
-        uint16_t opcode = fetch();
-        uint16_t op  = decode(opcode, "op");
-        uint16_t X   = decode(opcode, "X");   // index to lookup reg V0-VF
-        uint16_t Y   = decode(opcode, "Y");   // index to lookup reg V0-VF
-        uint16_t N   = decode(opcode, "N");   // 4-bit num
-        uint16_t NN  = decode(opcode, "NN");  // 8-bit immediate num
-        uint16_t NNN = decode(opcode, "NNN"); // 12-bit immediate mem addr
+        u_int16_t opcode = fetch();
+        u_int16_t op  = decode(opcode, "op");
+        u_int16_t X   = decode(opcode, "X");   // index to lookup reg V0-VF
+        u_int16_t Y   = decode(opcode, "Y");   // index to lookup reg V0-VF
+        u_int16_t N   = decode(opcode, "N");   // 4-bit num
+        u_int16_t NN  = decode(opcode, "NN");  // 8-bit immediate num
+        u_int16_t NNN = decode(opcode, "NNN"); // 12-bit immediate mem addr
         switch (op) {
             case 0x0:
                 if (X == 0) {
-                    if (NN == 0xE0) {
-                        system("clear");
-                        // Clear the screen
+                    if (NN == 0xE0) { // Clear the screen
+                        clear_display();
                     } else if (NN == 0xEE) {
                         // Return from a subroutine
                     }
@@ -202,10 +221,8 @@ void emulate() {
                 starting at the address stored in I.
                 Set VF to 01 if any set pixels are changed to unset, and 00 otherwise.
                 */
-                for (uint8_t curr = 0; curr < N; curr += 1) {
-                    DISPLAY[CHIP8.VR[X] + curr][CHIP8.VR[Y]] = 1;
-                }
-                draw_display();
+                draw_display(X, Y, N);
+				draw();
                 break;
             case 0xE:
                 break;
@@ -215,11 +232,6 @@ void emulate() {
                 fprintf(stderr, "unknown op %X\n", op);
                 break;
         }
-        // REMOVEME:
-        if (i == 2) {
-            //break;
-        }
-        i += 1;
     }
 }
 
